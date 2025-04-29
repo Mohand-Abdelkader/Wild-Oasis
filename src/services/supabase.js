77bb4bdb9f546714df86b7c3be666eb89ae9b763
Mohand Abdelkader <mohand27m@gmail.com>
2025-04-30 01:59:22 +0300

    supabase setup

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://baysgcuwqxefxwdpmypo.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJheXNnY3V3cXhlZnh3ZHBteXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NjA4MTMsImV4cCI6MjA2MTUzNjgxM30.CmMMsQftTVmNqGCji4CioymScgXAfp25u0s1MhMAT90";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

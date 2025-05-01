3e08133e1bf0e120c33adbb8478e754561e3308e
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-02 02:13:10 +0300

    update seeting

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -2,6 +2,7 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
   const {
@@ -13,17 +14,37 @@ function UpdateSettingsForm() {
       breakfastPrice,
     } = {},
   } = useSettings();
+  const { isUpdating, updateSetting } = useUpdateSetting();
 
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
@@ -31,6 +52,8 @@ function UpdateSettingsForm() {
       </FormRow>
       <FormRow label="Breakfast price">
         <Input
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,19 @@
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+
+export function useUpdateSetting() {
+  const queryClint = useQueryClient();
+  const { isLoading: isUpdating, mutate: updateSetting } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("setting Successfully Edited");
+      queryClint.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { isUpdating, updateSetting };
+}

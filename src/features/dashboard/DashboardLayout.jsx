4db9184e15fc6b1267ac359b810255c5e1bb3ce2
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-07 07:22:22 +0300

    add calc nums to the dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,41 @@
 import styled from "styled-components";
-
+import Stats from "./Stats";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
+import { useCabins } from "../cabins/useCabins";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+
+  if (isLoading || isLoading2 || isLoading3) return <Spinner />;
+  console.log(bookings);
+  console.log(stays);
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        numCabins={cabins.length}
+      />
+      <div>Statics</div>
+      <div>Today'</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
